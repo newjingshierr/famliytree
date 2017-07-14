05fc53ee93212358b08f2b6c259b99c050088f1b
enzo <jingshierr@126.com>
2017-07-14 17:40:43 +0800

    display center by text-align

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,49 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {Layout, Input, Icon} from 'antd';
+const {Header, Footer, Content} = Layout;
+const Search = Input.Search;
+require.ensure([], () => {
+    require("./themes/layout.less");
+}, "layout.css");
 
 
-var NotesList = React.createClass({
-    render: function() {
-        return (
-            <ol>
-                {
-                    this.props.children.map(function (child) {
-                        return <li>{child}</li>
-                    })
-                }
-            </ol>
-        );
+export class App extends React.Component<any,any> {
+    constructor() {
+        super();
     }
-});
 
-ReactDOM.render(<div>
-        <NotesList>
-            <span>hello</span>
-            <span>world</span>
-        </NotesList>
-    </div>
-    ,document.getElementById("example"))
+    onKeyChange = (e) => {
+        console.log(e.target.value)
+    }
+    onSearch = (value) => {
+        console.log(value);
+    }
+
+    render() {
+        return <div>
+            <Layout>
+                <Header className="layoutPosition" style={{textAlign:'center'}}>
+                        <div >
+                            <Search
+                                id="indexSearch"
+                                onChange={this.onKeyChange}
+                                placeholder="搜索"
+                                style={{ width: 200 }}
+                                onSearch={this.onSearch}
+                            />
+
+                            <Icon type="heart-o" style={{fontSize:24,marginLeft:'10',verticalAlign:'middle'}}/>
+                            <Icon type="user" style={{fontSize:24,marginLeft:'10',verticalAlign:'middle'}}/>
+                        </div>
+
+                </Header>
+                <Content style={{ height:1500}}>Content</Content>
+                <Footer>Footer</Footer>
+            </Layout>
+        </div>;
+    }
+}
+
+ReactDOM.render(
+    <App/>, document.getElementById("example"));
